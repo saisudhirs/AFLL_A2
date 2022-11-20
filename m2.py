ac8afe27ed52928fdddfa6e32958de7f261f3617
Samarth <samarthr1@outlook.com>
2022-11-20 21:31:06 +0530

    add in some support

--- m2.py
+++ m2.py
@@ -1,44 +1,91 @@
 import lex
 import yacc
 
-tokens = (
+
+keywords = ('IF','ELSE')        
+tokens = keywords +  (
     'CIO',
     'CIC',
     'CUO',
     'CUC',
     'NUM',
-    'VAR',
     'COMMA',
     'SP',
-    'IF',
-    'ELSE'
+    'VAR',
+    'LOG'
 )
 
+t_IF = r'if'
+t_ELSE = r'else'
+
 t_NUM = r'([0-9]*\.[0-9]*)|([0-9]+)'
-t_VAR = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_CIO = r'\('
 t_CIC = r'\)'
 t_CUO = r'\{'
 t_CUC = r'\}'
 t_COMMA = r'\,'
-t_IF = 'if'
-t_ELSE = 'else'
+t_LOG = r'&&|\|\||<|>|<=|>=|==|!='
+
+
+def t_VAR(s):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    if s.value.upper() in keywords:
+        s.type = s.value.upper()
+    return s
 
 
 def t_SP(s):
     r"""\s"""
     pass
 
+def p_body(p):
+    """body : if 
+    | ifelse
+    | empty"""
+    p[0] = p[1]
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+def p_expr(p):
+    """expr : NUM
+    | VAR
+    | func"""
 
-start = 'if'
+def p_args(p):
+    """args : expr
+    | expr COMMA args"""
+
+def p_func(p):
+    """func : VAR CIO args  CIC"""
+    p[0] = p[1]
+
+def p_term(p):
+    """term : expr LOG expr 
+    | expr
+    | CIO term CIC"""
+    p[0] = ''
+
+def p_if(p):
+    """if : IF CIO term CIC CUO body CUC"""
+    p[0] = p[3]
+
+def p_ifelse(p):
+    """ifelse : if ELSE CUO body CUC"""
+
+def p_empty(p):
+    """empty : """
+    pass
+
+def p_error(p):
+    print("Syntax error at ", p)
+    pass
+
 
 lexer = lex.lex()
-yacc = yacc.yacc()
-
-res = yacc.parse("if (func()) {}")
-print(res)
+yacc = yacc.yacc(start='body')
+try:
+    res = yacc.parse("if (f(g(x), 1) && 1) {}", debug=False)
+except Exception as  e:
+    print("Syntax Error")
